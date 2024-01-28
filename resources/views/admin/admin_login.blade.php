3f705555022884e7def2be3733331bbdaccf353d
mr_shah_4444 <devshah1234444@gmail.com>
2024-01-28 16:40:18 +0530

    adminlogin page desing

---
+++ resources/views/admin/admin_login.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="post">
+        <label for="nmae">username</label>
+        <input type="text" name="username">
+
+
+        <label for="nmae">password</label>
+        <input type="text" name="password">
+
+        <input type="button" value="sumbit">
+
+    </form>
+
+</body>
+
+</html>
